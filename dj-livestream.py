c3e8f84f87948af6d26a3a0196c2cfd07d64ada5
pseudozach <git@pseudozach.com>
2022-01-03 22:10:47 -0800

    add animations with threshold

--- dj-livestream.py
+++ dj-livestream.py
@@ -7,11 +7,15 @@ import os
 # from PIL import Image
 import json
 # import sseclient
+from contextlib import contextmanager
 
-url         = "https://legend.lnbits.com"
+url         = ""
 invoicekey  = ""
 interval    = 30
 source_name = "djlscomment"
+animation_one = os.path.dirname(__file__) + "/images/bitcoin.gif"
+animation_two = os.path.dirname(__file__) + "/images/rocket.gif"
+tip_threshold = 100
 
 # ------------------------------------------------------------
 
@@ -20,6 +24,10 @@ def update_text():
 	global invoicekey
 	global interval
 	global source_name
+
+	if invoicekey == "":
+		print("invoicekey is required")
+		return
 
 	# source = obs.obs_get_source_by_name(source_name)
 	# if source is not None:
@@ -80,6 +88,9 @@ def update_comment():
 	global invoicekey
 	global interval
 	global source_name
+	global animation_one
+	global animation_two
+	global tip_threshold
 
 	try:
 		livestreamurl = url + "/api/v1/payments"
@@ -89,22 +100,30 @@ def update_comment():
 		response = urlopen(req)
 		data = response.read()
 		text = data.decode('utf-8')
-		print("update_comment got response text " + text)
+		# TODO payments endpoint returns huge amount of invoices - find a way to limit it
+		# print("update_comment got response text " + text)
 		invoices = json.loads(text)
 		print("invoices[0] " + str(invoices[0]))
 
 		# get comment of last payment
 		lastcomment = ""
+		animationtoshow = animation_one
 		for invoice in invoices:
 			if invoice["amount"] > 0 and invoice["pending"] == False and invoice["extra"] and invoice["extra"]["comment"] and len(invoice["extra"]["comment"]) > 0:
 				print("found an incoming invoice with comment: " + invoice["extra"]["comment"])
 				lastcomment = invoice["extra"]["comment"]
+				if invoice["amount"] > tip_threshold:
+					animationtoshow = animation_two
 				break
 
-		settings = obs.obs_data_create()
-		# set text to text source - original method
-		print("should be setting the text to " + lastcomment)
+		print("setting comment text to " + lastcomment)
 		create_text_source(lastcomment)
+
+		print("setting tip animation to " + animationtoshow)
+		show_image_source(animationtoshow)
+
+		# settings = obs.obs_data_create()
+		# set text to text source - original method
 		# obs.obs_data_set_string(settings, "text", lastcomment)
 
 		# update existing source
@@ -112,6 +131,8 @@ def update_comment():
 		# obs.obs_source_update(source, settings)
 		# obs.obs_data_release(settings)
 		# obs.obs_source_release(source)
+
+		# check tip amount and show standard of large tip animation
 
 	except urllib.error.URLError as err:
 		obs.script_log(obs.LOG_WARNING, "Error opening URL '" + url + "': " + err.reason)
@@ -131,20 +152,30 @@ def script_update(settings):
 	global invoicekey
 	global interval
 	global source_name
+	global animation_one
+	global animation_two
+	global tip_threshold
 
 	url         = obs.obs_data_get_string(settings, "url")
 	invoicekey  = obs.obs_data_get_string(settings, "invoicekey")
 	interval    = obs.obs_data_get_int(settings, "interval")
 	source_name = obs.obs_data_get_string(settings, "source")
+	animation_one = obs.obs_data_get_string(settings, "animationone")
+	animation_two = obs.obs_data_get_string(settings, "animationtwo")
+	tip_threshold  = obs.obs_data_get_int(settings, "tipthreshold")
 
 	obs.timer_remove(update_comment)
 	# no need to keep writing the QR code as it does not change!
 	# if url != "" and source_name != "":
-	print("starting timer to update lnurl-pay comments every " + str(interval) + " seconds")
+	# print("starting timer to update lnurl-pay comments every " + str(interval) + " seconds")
 	obs.timer_add(update_comment, interval * 1000)
 
 def script_defaults(settings):
+	obs.obs_data_set_default_string(settings, "url", "https://legend.lnbits.com")
 	obs.obs_data_set_default_int(settings, "interval", 30)
+	obs.obs_data_set_default_int(settings, "tip_threshold", 100)
+	obs.obs_data_set_default_string(settings, "animation_one", os.path.dirname(__file__) + "/images/bitcoin.gif")
+	obs.obs_data_set_default_string(settings, "animation_two", os.path.dirname(__file__) + "/images/rocket.gif")
 
 def script_properties():
 	props = obs.obs_properties_create()
@@ -152,7 +183,10 @@ def script_properties():
 	obs.obs_properties_add_text(props, "url", "LNBits URL", obs.OBS_TEXT_DEFAULT)
 	obs.obs_properties_add_text(props, "invoicekey", "LNBits Invoice Key", obs.OBS_TEXT_DEFAULT)
 	obs.obs_properties_add_int(props, "interval", "Update Interval (seconds)", 5, 3600, 1)
-
+	obs.obs_properties_add_text(props, "animationone", "Regular Tip Animation File Location", obs.OBS_TEXT_DEFAULT)
+	obs.obs_properties_add_int(props, "tipthreshold", "Large Tip Threshold (sats)", 1, 1000000, 1)
+	obs.obs_properties_add_text(props, "animationtwo", "Large Tip Animation File Location", obs.OBS_TEXT_DEFAULT)
+	
 	# p = obs.obs_properties_add_list(props, "source", "Text Source", obs.OBS_COMBO_TYPE_EDITABLE, obs.OBS_COMBO_FORMAT_STRING)
 	# sources = obs.obs_enum_sources()
 	# print("init sources length " + str(len(sources)))
@@ -170,8 +204,13 @@ def script_properties():
 	return props
 
 def create_text_source(lastcomment):
-	sources = obs.obs_enum_sources()
-	print("create_text_source sources length " + str(len(sources)))
+	previouscomment = print_private_data("private1")
+	if previouscomment == lastcomment:
+		print("same comment so don't do anything")
+		return
+
+	# sources = obs.obs_enum_sources()
+	# print("create_text_source sources length " + str(len(sources)))
 
 	# if sources is not None:
 	source = obs.obs_get_source_by_name("djlscomment")
@@ -182,24 +221,28 @@ def create_text_source(lastcomment):
 		current_scene = obs.obs_frontend_get_current_scene()
 		scene = obs.obs_scene_from_source(current_scene)
 
-		# set the placeholder text
+		# set the text
 		settings = obs.obs_data_create()
 		obs.obs_data_set_string(settings, "text", lastcomment)
 
 		# obs_source_create_private not
-		newsource = obs.obs_source_create("text_ft2_source", "djlscomment", settings)
+		newsource = obs.obs_source_create("text_ft2_source", "djlscomment", settings, None)
 		obs.obs_scene_add(scene, newsource)
 		print("djlscomment comment added to scene")
 		obs.obs_scene_release(scene)
 		obs.obs_source_release(newsource)
 		obs.obs_data_release(settings)
 	else:
-		print("djlscomment already exists setting the text")
+		# print("djlscomment already exists setting the text")
 		settings = obs.obs_data_create()
 		obs.obs_data_set_string(settings, "text", lastcomment)
 		obs.obs_source_update(source, settings)
 		obs.obs_data_release(settings)
-		obs.source_list_release(sources)
+		obs.obs_source_release(source)
+		
+	write_private_data(lastcomment, "private1")
+
+	# obs.source_list_release(sources)
 
 	# obs is insane - thinks a source exists even when removed.
 	# djlssource = obs.obs_get_source_by_name("djlscomment")
@@ -222,4 +265,73 @@ def create_text_source(lastcomment):
 		# 	print("sources loop " + name)
 		# 	# obs.obs_property_list_add_string(p, name, name)
 
-	
+def show_image_source(animationtoshow):
+	if animationtoshow == "":
+		print("no animation file to show")
+		return
+
+	previousanimation = print_private_data("private2")
+	if previousanimation == animationtoshow:
+		print("same animation so don't do anything")
+		return
+
+	source_name = "djlsanimation"
+
+	# if sources is not None:
+	source = obs.obs_get_source_by_name(source_name)
+	# print("djlscomment source exists? " + str(source))
+	# print(source)
+	if source is None:
+		print(source_name + " source does not exist so create it")
+		current_scene = obs.obs_frontend_get_current_scene()
+		scene = obs.obs_scene_from_source(current_scene)
+
+		# set the image
+		settings = obs.obs_data_create()
+		obs.obs_data_set_string(settings, "file", animationtoshow)
+
+		# obs_source_create_private not
+		newsource = obs.obs_source_create("image_source", source_name, settings, None)
+		obs.obs_scene_add(scene, newsource)
+		print(source_name + " added to scene")
+		obs.obs_scene_release(scene)
+		obs.obs_source_release(newsource)
+		obs.obs_data_release(settings)
+	else:
+		print(source_name + " already exists setting it")
+		source_id = obs.obs_source_get_unversioned_id(source)
+		settings = obs.obs_data_create()
+		obs.obs_data_set_string(settings, "file", animationtoshow)
+		obs.obs_source_update(source, settings)
+		obs.obs_data_release(settings)
+		obs.obs_source_release(source)
+
+	write_private_data(animationtoshow, "private2")
+
+def send_to_private_data(data_type, field, result):
+    settings = obs.obs_data_create()
+    set = getattr(obs, f"obs_data_set_{data_type}")
+    set(settings, field, result)
+    obs.obs_apply_private_data(settings)
+    obs.obs_data_release(settings)
+
+def write_private_data(datatowrite, field):
+    result = datatowrite
+	# "__private1__"
+    send_to_private_data("string", field, result)
+
+@contextmanager
+def p_data_ar(data_type, field):
+    settings = obs.obs_get_private_data()
+    get = getattr(obs, f"obs_data_get_{data_type}")
+    try:
+        yield get(settings, field)
+    finally:
+        obs.obs_data_release(settings)
+
+def print_private_data(field):
+	data = ""
+	with p_data_ar("string", field) as value:
+		# print("got data " + field + " " + value)
+		data = value
+	return data
